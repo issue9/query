8c2159c17792d89fcf3f235aa0f64df71d6c53d6
caixw <xnotepad@gmail.com>
2020-11-01 00:40:20 +0800

    test: 删除不再需要的测试用例

--- query_test.go
+++ query_test.go
@@ -2,12 +2,7 @@
 
 package query
 
-import (
-	"fmt"
-	"testing"
-
-	"github.com/issue9/assert"
-)
+import "fmt"
 
 type State int
 
@@ -79,27 +74,4 @@ func (obj *testQueryObject) SanitizeQuery(errors Errors) {
 		errors.Add("int", "取值错误1")
 		errors.Add("int", "取值错误2")
 	}
-}
-
-func TestErrors(t *testing.T) {
-	a := assert.New(t)
-
-	errs := Errors{}
-
-	a.Panic(func() {
-		errs.Add("key")
-	})
-
-	a.Panic(func() {
-		errs.Set("key")
-	})
-
-	errs.Add("key1", "v1", "v2")
-	a.Equal(errs, map[string][]string{"key1": {"v1", "v2"}})
-
-	errs.Add("key1", "v1", "v3")
-	a.Equal(errs, map[string][]string{"key1": {"v1", "v2", "v1", "v3"}})
-
-	errs.Set("key1", "v1")
-	a.Equal(errs, map[string][]string{"key1": {"v1"}})
 }
