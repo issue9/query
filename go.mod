40da57f63834e35a9dfab41d54480952f2bf1bc1
caixw <xnotepad@gmail.com>
2020-11-01 00:39:29 +0800

    build: 引用 validation.Messages 作为 Errors 类型

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/issue9/query/v2
 require (
 	github.com/issue9/assert v1.4.1
 	github.com/issue9/conv v1.2.0
+	github.com/issue9/validation v0.1.0
 )
 
 go 1.13
--- query.go
+++ query.go
@@ -37,6 +37,8 @@
 // 中指定的 []int{1,2}。
 package query
 
+import "github.com/issue9/validation"
+
 // Tag 在 struct tag 的标签名称
 const Tag = "query"
 
@@ -71,22 +73,4 @@ type Unmarshaler interface {
 // Errors 表示一组错误信息的集合
 //
 // 键名查询参数名称，键值则为在解析和验证过种中返回的错误信息。
-type Errors map[string][]string
-
-// Add 为查询参数 key 添加一条新的错误信息
-func (err Errors) Add(key string, val ...string) {
-	if len(val) == 0 {
-		panic("参数 val 必须指定")
-	}
-
-	err[key] = append(err[key], val...)
-}
-
-// Set 将查询参数 key 的错误信息改为 val
-func (err Errors) Set(key string, val ...string) {
-	if len(val) == 0 {
-		panic("参数 val 必须指定")
-	}
-
-	err[key] = val
-}
+type Errors = validation.Messages
